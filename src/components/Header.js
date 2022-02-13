427a966162b7e4c137756d7cec304a8bc3099c32
Njegos Petrovic <pnjegosh@gmail.com>
2022-02-13 15:12:27 +0100

    Initial Commit.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "../App.css";
+
+const Header = () => {
+  return (
+    <div className="headerContainer">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/country/Country.js
@@ -0,0 +1,12 @@
+import "./Country.css";
+import Header from "../components/Header";
+
+function Country() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Country;
